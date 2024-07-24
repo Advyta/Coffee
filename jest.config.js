c17b5ee84e1a1326218ed7082395b7b9d7f664ae
Advyta <advytajs@gmail.com>
2024-07-24 16:57:07 +0200

    Footer updated

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest"
-  },
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-};
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
+
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="py-8">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:mb-0">
 
-export default Footer
+            <p className='body-s'>© {new Date().getFullYear()} Your Company. All rights reserved.</p>
+          </div>
+          <div className="flex justify-evenly">
+            <Link href='/' className="text-inactive-text hover:text-active-text p-2" >
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </Link>
+            <Link href='/' className="text-inactive-text hover:text-active-text p-2" >
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </Link>
+            <Link href='/' className="text-inactive-text hover:text-active-text p-2" >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </Link>
+            <Link href='/' className="text-inactive-text hover:text-active-text p-2" >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
